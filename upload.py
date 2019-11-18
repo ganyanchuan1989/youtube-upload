9672ecfca3643ec64349b7f19bd626b7b3f8b054
ganxunzou <ganxunzou@163.com>
2019-11-18 10:13:02 +0800

    update

--- upload.py
+++ upload.py
@@ -11,9 +11,12 @@ sys.path.insert(0, os.path.join(os.path.dirname(
     os.path.realpath(__file__)), os.pardir))
 os.environ["https_proxy"] = "https://127.0.0.1:1080"  # 增加代理
 
-BASE_PATH ="C:/workspace/py/amemv-crawler/download"
+BASE_PATH = "C:/workspace/py/amemv-crawler/download"
 PREFIX = "【抖音】【转载】"
-category = "Pets & Animals" # 娱乐
+category = "Pets & Animals"  # 娱乐
+
+v_count = 0
+
 
 class UploadWorker(Thread):
     def __init__(self, queue):
@@ -26,11 +29,18 @@ class UploadWorker(Thread):
             uploadVideo(user)
             self.queue.task_done()
 
+
 def uploadVideo(user):
-    v_path = "%s\\%s\\%s\\%s.mp4" % (BASE_PATH, user.tags, user.user_id, user.v_uri )
+    v_path = "%s\\%s\\%s\\%s.mp4" % (
+        BASE_PATH, user.tags, user.user_id, user.v_uri)
     main.run2(user.desc, user.desc, user.tags, v_path, category)
-    q = (ComUser.update(isupload = True).where(ComUser.user_id == user.user_id & ComUser.v_uri == user.v_uri))
+    q = ComUser.update(isupload=True).where(
+        (ComUser.user_id == user.user_id) & (ComUser.v_uri == user.v_uri))
     q.execute()
+    global v_count
+    v_count += 1
+    print(v_count)
+
 
 if __name__ == "__main__":
     content = []
@@ -40,21 +50,23 @@ if __name__ == "__main__":
     user_url_list = []
     for item in data:
         user_url_list.append(item["url"])
-        
-    query = ComUser.select().where((ComUser.user_url << user_url_list) & ComUser.isupload == False & ComUser.v_size != 0)
+
+    query = ComUser.select().where((ComUser.user_url << user_url_list) &
+                                   (ComUser.isupload == False) & (ComUser.v_size != 0))
+    # sql = query.sql()
+    # print(sql)
     for user in query:
         name = user.user_name
-        user.tags += " ," +  name
-        user.desc = PREFIX + "【" + name +"】" + user.desc
-        print(user)
-        # queue.put(user)
+        user.desc = PREFIX + "【" + name + "】" + user.desc
+        # print(user)
+        queue.put(user)
         break
 
     if(queue.empty()):
         print('no need upload')
-    
-    else: 
-        # 10 Thread 
+
+    else:
+        # 10 Thread
         for i in range(1, 11):
             w = UploadWorker(queue)
             w.daemon = True
@@ -63,5 +75,3 @@ if __name__ == "__main__":
         queue.join()
 
         print("Upload All Video Done")
-
-    
